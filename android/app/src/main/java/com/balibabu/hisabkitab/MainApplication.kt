23bc0031acf6ef5342acc03282e4586931019efc
balibabu <balibabuchauhan1@gmail.com>
2026-05-10 23:07:15 +0545

    changed package name to hisabkitab and added signup page

--- android/app/src/main/java/com/balibabu/hisabkitab/MainApplication.kt
+++ android/app/src/main/java/com/balibabu/hisabkitab/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.balibabu.khatabook
+package com.balibabu.hisabkitab
 
 import android.app.Application
 import com.facebook.react.PackageList
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,7 +1,11 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
-import { EmailAuthProvider, getAuth, onAuthStateChanged, reauthenticateWithCredential, signInWithEmailAndPassword, signOut, updatePassword } from '@react-native-firebase/auth';
+import {
+    EmailAuthProvider, getAuth, onAuthStateChanged, reauthenticateWithCredential,
+    signInWithEmailAndPassword, signOut, updatePassword, createUserWithEmailAndPassword
+} from '@react-native-firebase/auth';
 import MyIndicator from '../components/MyIndicator';
 import LoginScreen from '../screens/login/LoginScreen';
+import SignupScreen from '../screens/login/SignupScreen';
 
 const AuthContext = createContext();
 const auth = getAuth();
@@ -9,6 +13,7 @@ const auth = getAuth();
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [initializing, setInitializing] = useState(true);
+    const [isLoginPage, setIsLoginPage] = useState(true);
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, u => { setUser(u); setInitializing(false); });
@@ -16,6 +21,15 @@ export const AuthProvider = ({ children }) => {
     }, []);
     const login = (email, password) => signInWithEmailAndPassword(auth, email, password);
     const logout = () => signOut(auth);
+
+    const signup = async (email, password) => {
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            setUser(userCredential.user);
+        } catch (error) {
+            throw error;
+        }
+    };
 
     async function changePassword(currentPassword, newPassword) {
         const credential = EmailAuthProvider.credential(user.email, currentPassword);
@@ -26,7 +40,9 @@ export const AuthProvider = ({ children }) => {
 
     return (
         <AuthContext.Provider value={{ user, logout, changePassword }}>
-            {initializing ? <MyIndicator text={'loading user info'} /> : user ? children : <LoginScreen login={login} />}
+            {initializing ? (<MyIndicator text="loading user info" />) : user ? (children) : isLoginPage ?
+                (<LoginScreen login={login} setIsLoginPage={setIsLoginPage} />) :
+                (<SignupScreen signup={signup} setIsLoginPage={setIsLoginPage} />)}
         </AuthContext.Provider>
     );
 };
